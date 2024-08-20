c3ef2faa05755f71b68a451bdfb42bf4fd38fb4b
fritzrehde <fritzrehde.work@gmail.com>
2024-08-20 21:40:24 +1000

    updated kanshi config to reflect updated external display name

--- scripts/fzf-cd.sh
+++ scripts/fzf-cd.sh
@@ -10,4 +10,4 @@ base_path=.
 new_path=$(fd --type d --base-directory $search_path | fzf.sh +m -0)
 
 [ -n "$new_path" ] \
-	&& tmux send-keys -t "$current_window" "cd $base_path/$new_path; clear" ENTER
+	&& tmux send-keys -t "$current_window" "cd '$base_path/$new_path'; clear" ENTER
